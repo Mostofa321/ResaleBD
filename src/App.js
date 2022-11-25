ce331a3b9868dd83744bccadbe72c6b0e8b0dd16
Mostofa321 <hmshumon123@gmail.com>
2022-11-25 07:50:02 +0600

    installed dasy ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello world!
-    </h1>
+        Hello world!
+      </h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }

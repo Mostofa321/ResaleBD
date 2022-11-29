fc222c7f707ac844b9c919a5f8e585d894a22b17
Mostofa321 <hmshumon123@gmail.com>
2022-11-29 18:32:16 +0600

    loaded all availabe products from mongodb by using react query

--- src/Pages/Home/AdvertisedProducts.js
+++ src/Pages/Home/AdvertisedProducts.js
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
 const AdvertisedProducts = () => {
-    const [availableProducts, setAvailableProducts] = useState([]);
-    useEffect(() => {
-        fetch('phones.json')
-            .then(res => res.json())
-            .then(data => setAvailableProducts(data))
-    }, [])
+    // const [availableProducts, setAvailableProducts] = useState([]);
+    // useEffect(() => {
+    //     fetch('phones.json')
+    //         .then(res => res.json())
+    //         .then(data => setAvailableProducts(data))
+    // }, []);
+
+    const { data: availableProducts = [], isLoading } = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/products');
+            const data = await res.json();
+            return data
+        }
+    });
+ 
+    if (isLoading) {
+        return <div>Loading.....</div>
+    }
+
     return (
         <div className='container mx-auto my-10'>
             <h1 className='text-center text-4xl'>Available Products</h1>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 bg-green-100 p-10'>
                 {
                     availableProducts.map(availableProduct => {
-                        const { id, image, name, location, resalePrice, originalPrice, yearsOfUse, postTime, sellerName } = availableProduct
+                        const { _id, image, name, location, resalePrice, originalPrice, yearsOfUse, postTime, sellerName } = availableProduct
                         return (
-                            <div className="card w-96 bg-base-100 shadow-xl" key={id}>
+                            <div className="card w-96 bg-base-100 shadow-xl" key={_id}>
                                 <figure className="px-10 pt-10">
                                     <img src={image} alt="Shoes" className="rounded-xl" />
                                 </figure>

56fc6daef63e3df3e8c80a15fb94f104b1cc4b8c
Mostofa321 <hmshumon123@gmail.com>
2022-11-30 02:31:06 +0600

    The sign up form has been slightly modified

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -6,7 +6,7 @@ import { auth, AuthContext } from '../../Contexts/AuthProvider';
 
 const SignUp = () => {
     const navigate = useNavigate()
-    const { signUp, googleLogin } = useContext(AuthContext);
+    const { signUp } = useContext(AuthContext);
     const [errMessage, setErrMessage] = useState(null)
     const [successMessage, setSuccesMeassage] = useState(null)
     const createUser = (e) => {
@@ -16,7 +16,8 @@ const SignUp = () => {
         const email = form.email.value
         const password = form.password.value;
         const userPhoto = form.userPhoto.value;
-        if (!name || !email || !password || !userPhoto) {
+        const userRole = form.select.value;
+        if (!name || !email || !password || !userPhoto || !userRole) {
             return
         }
         signUp(email, password)
@@ -43,18 +44,7 @@ const SignUp = () => {
             })
             .catch(err => console.log(err))
     }
-    // sign up with google 
-    const signUpGoogle = () => {
-        googleLogin()
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-                navigate('/')
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
+   
     return (
         <div className='pt-5 container flex mx-auto my-10'>
             <form onSubmit={createUser} className='mx-auto p-5 w-11/12 md:w-5/12' style={{ boxShadow: "rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px" }}>
@@ -71,18 +61,16 @@ const SignUp = () => {
                 <div className="mb-3">
                     <input type="url" name='userPhoto' className="input input-bordered w-full" placeholder='enter your photo url' />
                 </div>
+                <select name='select' className="select select-bordered w-full mb-3">
+                    <option disabled selected>buyer</option>
+                    <option>saler</option>
+                </select>
 
                 {
                     successMessage ? <p className='text-success'>{successMessage}</p> : <p className='text-danger'>{errMessage}</p>
                 }
 
                 <button type="submit" className="btn btn-primary w-full">Sign Up</button>
-                <div className='flex items-center'>
-                    <hr className=' mr-4' style={{ border: "1px solid", width: "45%", height: "1px" }} />
-                    <h3 className='text-lg'>or</h3>
-                    <hr className=' ml-4' style={{ border: "1px solid", width: "45%", height: "1px" }} />
-                </div>
-                <button onClick={signUpGoogle} type="submit" className="btn btn-secondary w-full">Sign Up With Google <i className="fa-brands fa-google ml-2 text-lg"></i></button>
                 <p className='mt-5'><small className='mr-3'>Already have an account?</small><Link to='/login'>Sign In</Link></p>
             </form>
         </div>
